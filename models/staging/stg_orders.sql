c4b04dcf908a2a4d7ea46af71e9247144b749161
Priyashree Bhadra <bhadrap812@gmail.com>
2025-02-18 04:16:34 +0000

    added seed, packages

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,5 @@
 select
+    {{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as stg_key,
     p.productid, 
     p.productname, 
     p.category,
@@ -10,10 +11,12 @@ select
     o.orderdate,
     o.orderid,
     o.shipdate,
-    o.ordercostprice,
-    o.ordersellingprice,
-    {{markdown('ordersellingprice','ordercostprice')}} as markdown,
-    o.ordersellingprice-o.ordercostprice as profit
+    o.ordercostprice as ordercostprice,
+    o.ordersellingprice as ordersellingprice,
+    o.ordersellingprice-o.ordercostprice as profit,
+    {{ markup('ordersellingprice','ordercostprice')}} as markup,
+    d.delivery_team as delivery_team
 from {{ ref('raw_orders') }} as o 
 left join {{ ref('raw_customer') }} as c on o.customerid=c.customerid
 left join {{ ref('raw_product') }} as p on o.productid=p.productid
+left join {{ ref('delivery_team') }} as d on o.shipmode=d.shipmode
